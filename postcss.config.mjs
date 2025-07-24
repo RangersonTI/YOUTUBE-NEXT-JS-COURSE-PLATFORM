cfe1d27cb260bb2d3c02d2d3c67e338225fa5cca
Rangerson Clemente <rangerson14@outlook.com>
2025-07-23 22:20:20 -0300

    fazendo configuração do tailwind css

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,21 @@
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  content:["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors:{
+        text: 'var(--color-text)',
+        error: 'var(--color-error)',
+        paper: 'var(--color-paper)',
+        primary: 'var(--color-primary)',
+        backgroud: 'var(--color-backgroud)',
+        'primary-contrast': 'var(--color-primary)'
+      }
+    }
+  },
+  plugins: {
+    "@tailwindcss/postcss":{}
+  },
 };
 
 export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-text">
       Home
     </main>
   );
